296c592d930ebb46e74a7558cbdc15a72c717a40
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-27 21:48:33 -0500

    trying to fix manifest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "whats_the_tea_sis?_production"
+  # config.active_job.queue_name_prefix = "whats_the_tea_sis_production"
 
   config.action_mailer.perform_caching = false
 
