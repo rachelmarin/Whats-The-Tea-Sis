17ee50f52bdd4836faa7f389a4e5d2d41c12efc3
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-27 20:49:53 -0500

    filled in recipe relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,18 @@
 class Recipe < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true
+    belongs_to :category
+    has_many :comments
+    has_many :users, through: :comments
+    has_many :equipments
+    has_many :ingredients 
+  
+    accepts_nested_attributes_for :ingredient
+  accepts_nested_attributes_for :equipment
+  accepts_nested_attributes_for :category, reject_if: :all_blank
+
+  def category_name
+    self.category ? self.category.name : "category not available"
+  end
+    
 end
