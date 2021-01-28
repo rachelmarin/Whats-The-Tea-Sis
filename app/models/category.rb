993121ee4af3c1819a379c4d3ecbc413652f122c
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-27 20:48:59 -0500

    filled in category relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    has_many :recipes
+
+    validates :name, presence: true, uniqueness: true
+  
+    accepts_nested_attributes_for :recipes, reject_if: :all_blank  
 end
