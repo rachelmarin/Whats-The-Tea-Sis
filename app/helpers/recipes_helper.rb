51b8383ded7bf1644d57e47e723de129d0692816
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-28 15:49:48 -0500

    filled in recipe helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+    def category_name(recipe)
+      if recipe.category
+        recipe.category.name
+      else
+        link_to "Add Category", edit_recipe_path(recipe)
+    end
+  end   
 end
