5976159d5f6baba9070d7a7063488358b03548a8
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-27 20:47:29 -0500

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
+  
+  root(to: "static#home")
+  
   devise_for :users
   resources :recipes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :categories
+ 
 end
