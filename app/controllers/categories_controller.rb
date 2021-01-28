7341e6d561be11c99d79958925440dfaacb692fd
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-27 22:47:31 -0500

    added create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,5 +8,16 @@ class CategoriesController < ApplicationController
         @category.recipes.build
         @category.recipes.build
       end
-           
+      
+      def create
+        @category = Category.new(category_params)
+    
+    
+        if @category.save
+          redirect_to recipes_path
+        else
+          render :new
+        end
+      end
+    
 end
