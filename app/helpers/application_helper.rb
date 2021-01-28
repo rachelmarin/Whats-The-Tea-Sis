086d4ee2cf3d617d50d8e734482ff53535db6108
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-28 15:49:28 -0500

    filled in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
-end
+    def render_nav_links
+      content_tag("ul", class: "right") do
+        if user_signed_in? # is logged in
+            content_tag("li") do
+              link_to("Recipes", recipes_path)
+            end + 
+            content_tag("li") do
+              link_to("Create Recipe", new_recipe_path)
+            end + 
+            content_tag("li") do
+              link_to("Logout", destroy_user_session_path, method: "DELETE")
+            end
+        else
+          content_tag("li") do
+            link_to("Login", new_user_session_path)
+          end + 
+          content_tag("li") do
+            link_to("Signup", new_user_registration_path)
+          end
+        end
+      end
+    end
+  end
