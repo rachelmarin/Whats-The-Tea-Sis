d9b9438acf0dc534e4429a3e94f4f153e04ad244
rachelmarin <Rachelmarinpal@gmail.com>
2021-01-28 23:52:25 -0500

    decided not to use the recipe helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,9 +1,3 @@
 module RecipesHelper
-    def category_name(recipe)
-      if recipe.category
-        recipe.category.name
-      else
-        link_to "Add Category", edit_recipe_path(recipe)
-    end
-  end   
+
 end
