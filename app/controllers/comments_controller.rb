4f37569ffa95b32f39f231889c26f471689f7a7f
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-01 00:39:03 -0500

    added comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+  
+class CommentsController < ApplicationController
+
+    def create
+      comment = Comment.create(comment_params)
+      redirect_to comment.recipe
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:content, :recipe_id, :user_id, user_attributes:[:username])
+    end
+  end
