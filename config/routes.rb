42f5f7a4c168666015621b76e19720e2255fbd76
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-02 03:11:15 -0500

    add omni

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   root(to: "static#home")
   
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :recipes
+  resources :comments
   resources :categories do
     resources :recipes, only: [:new, :create, :index]
   end
+
+  # match '/auth/:google_oath2/callback' => 'static#google', via:[:get, :post]
+  match '/auth/:facebook/callback' => 'static#facebook', via:[:get, :post]
 end
