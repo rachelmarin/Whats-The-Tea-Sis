658361f97801fc0313fd9e92c1aecbe4dd008d44
rachelmarin <Rachelmarinpal@gmail.com>
2021-02-04 10:03:35 -0500

    got cacoon working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
     has_many :recipes
 
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true
   
     accepts_nested_attributes_for :recipes, reject_if: :all_blank  
 end
